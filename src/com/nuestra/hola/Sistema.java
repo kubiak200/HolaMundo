49cdbb0b4f63d968a509c04d27ae7b770fc6257e
gisalito <gisalito@gisalitopc>
2015-06-22 01:44:35 -0300

    Le agregué el println Hola Mundo!!!

--- src/com/nuestra/hola/Sistema.java
+++ src/com/nuestra/hola/Sistema.java
@@ -3,7 +3,9 @@ package com.nuestra.hola;
 public class Sistema {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+
+
+		System.out.println("Hola Mundo!!!");
 
 	}
 
